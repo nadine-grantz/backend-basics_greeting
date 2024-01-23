78f69ee974b90513dbc5af037bf60c2f71e319fe
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-23 11:36:29 +0100

    add

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-// import { server } from "./server.js";
-
-// server.listen(…
+import { server } from "./server.js";
+// const server = require("./server");
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
